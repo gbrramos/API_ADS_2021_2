b84083d4a2df88a0220653b9fb903c119ad64c53
EduardoPereiraCarvalho <duedu.nda@gmail.com>
2021-10-01 08:08:37 -0300

    wip

---
+++ backend/Alocacoes/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import widgets
+from django import forms
+from django.db.models import fields
+from .models import Alocacao
+
+
+class AlocacoesForms(forms.ModelForm):
+
+    class Meta:
+        model =  Alocacao
+        fields = ('coloborador', 'posto', 'alocacao',)
--- backend/Alocacoes/models.py
+++ backend/Alocacoes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.db.models.fields import BooleanField
+from Colaboradores.models import Colaborador
+from PostosDeTrabalho.models import PostoDeTrabalho
 # Create your models here.
+
+class Alocacao(models.Model):
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.CASCADE)
+    posto = models.ForeignKey(PostoDeTrabalho, on_delete=models.CASCADE)
+    alocacao = models.BooleanField()
