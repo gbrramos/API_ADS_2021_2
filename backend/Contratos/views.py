5e64dca1dcb0788913f42203604a83f66b526e03
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2021-11-04 22:27:08 -0300

    wip

--- backend/Contratos/views.py
+++ backend/Contratos/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from .forms import ContratosForms
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import FileResponse
-import io
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch
-from reportlab.lib.pagesizes import letter
+from io import StringIO
+from io import BytesIO
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+from html import escape
 
 from .models import Contrato
 
@@ -60,30 +62,53 @@ def delete(request, id):
     return redirect('/contratos/lista')
 
 
+# @login_required
+# def gerarRelatorio(request):
+#     # Create Bytestream buffer
+#     buf = io.BytesIO()
+#     # Create a canvas
+#     c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+#     # Create a text object
+#     textob = c.beginText()
+#     textob.setTextOrigin(inch, inch)
+#     textob.setFont("Helvetica", 14)
+#     #Dados do Contrato
+
+#     #Add some lines of text
+#     lines = [
+#         "Guilherme",
+#         "Felipe",
+#         "Eduardo",
+#     ]
+
+#     for line in lines:
+#         textob.textLine(line)
+
+#     c.drawText(textob)
+#     c.showPage()
+#     c.save()
+#     buf.seek(0)
+#     return FileResponse(buf, as_attachment=True, filename='relatorio.pdf')
+
 @login_required
 def gerarRelatorio(request):
-    # Create Bytestream buffer
-    buf = io.BytesIO()
-    # Create a canvas
-    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
-    # Create a text object
-    textob = c.beginText()
-    textob.setTextOrigin(inch, inch)
-    textob.setFont("Helvetica", 14)
-    #Dados do Contrato
+    #Retrieve data or whatever you need
+    return render_to_pdf(
+            'contratos/model.html',
+            {
+                'pagesize':'A4',
+                'mylist': request,
+            }
+        )
 
-    #Add some lines of text
-    lines = [
-        "Guilherme",
-        "Felipe",
-        "Eduardo",
-    ]
 
-    for line in lines:
-        textob.textLine(line)
+def render_to_pdf(template_src, context_dict):
+    template = get_template(template_src)
+    # context = Context(context_dict)
+    html  = template.render(context_dict)
+    result = BytesIO()
 
-    c.drawText(textob)
-    c.showPage()
-    c.save()
-    buf.seek(0)
-    return FileResponse(buf, as_attachment=True, filename='relatorio.pdf')
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
